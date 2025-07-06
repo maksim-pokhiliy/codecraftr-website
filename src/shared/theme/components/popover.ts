b02a0b6455ad0b4e10d919e49903e6f58b996711
Maksim Pokhiliy <maksim.pokhiliy@gmail.com>
2025-07-06 13:24:01 +0000

    feat: technology stack section with interactive cards and popover descriptions

--- src/shared/theme/components/popover.ts
+++ src/shared/theme/components/popover.ts
@@ -4,9 +4,6 @@ import { Components, Theme } from "@mui/material";
 
 export const popover: Components<Theme>["MuiPopover"] = {
   styleOverrides: {
-    root: {
-      pointerEvents: "none",
-    },
     paper: ({ theme }) => ({
       maxWidth: theme.spacing(42),
       padding: theme.spacing(2),
